b4e54e78243f8c0574de99da98a71694dc6f2233
Siroshun09 <siro.shun8@gmail.com>
2023-10-11 17:15:58 +0900

    build: setup Gradle project

---
+++ build.gradle.kts
@@ -0,0 +1,54 @@
+plugins {
+    `java-library`
+    id("io.papermc.paperweight.userdev") version "1.5.10"
+    id("com.github.johnrengelman.shadow") version "8.1.1"
+}
+
+group = "net.okocraft.foliaregionvisualizer"
+version = "1.0-SNAPSHOT"
+
+val mcVersion = "1.20.2"
+val fullVersion = "${version}-mc${mcVersion}"
+
+repositories {
+    mavenCentral()
+    maven {
+        url = uri("https://jitpack.io")
+    }
+}
+
+dependencies {
+    paperweight.foliaDevBundle("$mcVersion-R0.1-SNAPSHOT")
+    compileOnly("com.github.BlueMap-Minecraft:BlueMapAPI:2.6.1")
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
+tasks {
+    reobfJar {
+        outputJar.set(
+                project.layout.buildDirectory
+                        .file("libs/FoliaRegionVisualizer-${fullVersion}.jar")
+        )
+    }
+
+    build {
+        dependsOn(reobfJar)
+    }
+
+    compileJava {
+        options.encoding = Charsets.UTF_8.name()
+        options.release.set(17)
+    }
+
+    processResources {
+        filteringCharset = Charsets.UTF_8.name()
+
+        filesMatching(listOf("plugin.yml")) {
+            expand("projectVersion" to fullVersion)
+        }
+    }
+}
