2a29c039c38bd8874eabba54cb7f8872cf010fe6
Siroshun09 <siro.shun8@gmail.com>
2024-12-04 22:28:02 +0900

    build: update bluemap-api

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,7 @@ repositories {
 
 dependencies {
     paperweight.foliaDevBundle("$mcVersion-R0.1-SNAPSHOT")
-    compileOnly("de.bluecolored.bluemap:BlueMapAPI:2.7.2")
+    compileOnly("de.bluecolored:bluemap-api:2.7.3")
 }
 
 paperweight.reobfArtifactConfiguration = io.papermc.paperweight.userdev.ReobfArtifactConfiguration.MOJANG_PRODUCTION
