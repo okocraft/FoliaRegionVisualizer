67af42a66923f8b872ecedec392be422e0b6d3a6
Siroshun09 <siro.shun8@gmail.com>
2025-05-24 16:55:46 +0900

    build: update paperweight-userdev to 2.0.0-beta.17

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     `java-library`
-    id("io.papermc.paperweight.userdev") version "1.7.7"
+    id("io.papermc.paperweight.userdev") version "2.0.0-beta.17"
 }
 
 group = "net.okocraft.foliaregionvisualizer"
